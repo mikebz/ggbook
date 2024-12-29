2898b4d6e9a31722e537524a7599f7e07a688f7c
Mike Borozdin <mikebz@google.com>
2024-12-29 15:20:05 -0500

    now specifying the address to listen on

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -27,46 +26,5 @@ func main() {
 		panic("failed to migrate")
 	}
 
-	r := http.NewServeMux()
-
-	fmt.Println("Starting the server")
-	r.HandleFunc("/", indexHandler)
-	r.HandleFunc("/guests/{id}", getGuestHandler)
-	r.HandleFunc("/guests", guestHandler)
-
-	http.ListenAndServe(":8080", r)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go Guest Book!\n")
-}
-
-// getGuestHandler handles requests to the /guests/{id} endpoint.
-// It supports GET (retrieve a guest), PUT (update a guest), and DELETE (delete a guest) methods.
-// Other methods will return a 405 Method Not Allowed error.
-func getGuestHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	fmt.Fprintf(w, "%s guest. ID: %s\n", r.Method, id)
-
-	switch r.Method {
-	case "GET":
-	case "PUT":
-	case "DELETE":
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
-// guestHandler handles requests to the /guests endpoint.
-// It supports GET (list all guests) and POST (create a new guest) methods.
-// Other methods will return a 405 Method Not Allowed error.
-func guestHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s guests\n", r.Method)
-
-	switch r.Method {
-	case "GET":
-	case "POST":
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
+	listenAndServe()
 }
