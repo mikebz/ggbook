22e8f383dfd75d2b75da9f02fa52a87a2b0d77e9
Mike Borozdin <mikebz@google.com>
2025-01-14 19:31:40 -0800

    front end chat messages working

--- README.md
+++ README.md
@@ -46,7 +46,46 @@ The server will listen on `localhost:8080` by default.  You can change this by s
     * `PUT`: Updates a specific guest by ID.  Requires a JSON payload with `name` and `email` fields.
     * `DELETE`: Deletes a specific guest by ID.
 
-## Testing
+## Front end development
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+
+### Type Support for `.vue` Imports in TS
+
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
+
+### Project setup
+
+All the front end GUI is in the /html folder and uses VueJS.  In order to 
+compile and run the front end please follow the standard `npm` instructions:
+
+```
+  npm install
+  npm run format
+  npm run dev
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
+
+
+## Testing APIs
 
 The project includes a `Makefile` with targets for running tests and making API calls using `curl`.
 
---
+++ html/src/App.vue
@@ -0,0 +1,125 @@
+<script lang="ts">
+import Message from './components/Message.vue'
+import ChatBox from './components/ChatBox.vue'
+
+interface Message {
+  text: string
+  id: number
+  isMine: boolean
+  author: string
+}
+
+export default {
+  name: 'App',
+
+  components: {
+    ChatBox,
+    Message,
+  },
+
+  created() { },
+  methods: {
+    onSubmit(event: SubmitEvent, text: string) {
+      event.preventDefault()
+
+      this.messages.push({
+        text: text,
+        id: this.messages.length + 1,
+        isMine: true,
+        author: this.user?.name,
+      })
+      fetch('/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: text,
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          this.messages.push({
+            text: data,
+            id: this.messages.length + 1,
+            isMine: false,
+            author: 'ai',
+          })
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+          // Add error message to chat
+          this.messages.push({
+            text: 'Sorry, there was an error processing your message',
+            id: this.messages.length + 1,
+            isMine: false,
+            author: 'System',
+          })
+        })
+    },
+  },
+  data: () => ({
+    user: { id: '1', name: 'human' },
+    messages: [] as Message[],
+  }),
+}
+</script>
+
+<template>
+  <div class="app">
+    <div ref="messages" class="messages">
+      <Message v-for="message in messages" :key="message.id" :class="['message', { right: message.isMine }]"
+        :dark="message.isMine" :text="message.text" :author="message.author" />
+    </div>
+
+    <ChatBox class="chat-box" @chat-message="onSubmit" />
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Tahoma', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+button {
+  border: 0;
+  background: #2a60ff;
+  color: white;
+  cursor: pointer;
+  padding: 1rem;
+}
+
+input {
+  border: 0;
+  padding: 1rem;
+  background: rgba(0, 0, 0, 0.1);
+}
+</style>
+
+<style scoped>
+.app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.messages {
+  flex-grow: 1;
+  overflow: auto;
+  padding: 1rem;
+}
+
+.message+.message {
+  margin-top: 1rem;
+}
+
+.message.right {
+  margin-left: auto;
+}
+</style>
